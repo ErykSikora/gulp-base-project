6bf19021b25cf9a9e979e89bfe7fcba9c0a77f71
ErykSikora <eryksikora95@gmail.com>
2020-09-24 16:17:45 +0200

    1.0

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      browserSync = require('browser-sync').create(),
+      prefix = require('autoprefixer'),
+      rename = require('gulp-rename'),
+      concat = require('gulp-concat'),
+      minifycss = require('gulp-clean-css'),
+      uglify = require('gulp-uglify-es').default,
+      deploy = require("ftp-deploy"),
+      ftp = require('vinyl-ftp');
+
+const ftpHost = {
+      host: 'xxx.xy',
+      user: 'xxx',
+      password: 'xxx',
+      parallel: 10
+};
+
+const dirHost = 'domains/xxx.xy/public_html';
+
+function scss(){
+  return gulp
+    .src('./dev/sass/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(rename('stylesheet.min.css'))
+    .pipe(minifycss())
+    .pipe(gulp.dest('./assets/css'))
+    .pipe(browserSync.stream());
+}
+
+function js(){
+  return gulp
+    .src(["./dev/js/**/*.js", "./dev/js/*.js"])
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('./assets/js'))
+    .pipe(rename('stylesheet.min.css'))
+    .pipe(uglify())
+    .pipe(rename('script.min.js'))
+    .pipe(gulp.dest('./assets/js'))
+    .pipe(browserSync.stream());
+}
+
+function deploy_css(){
+  let conn = ftp.create(ftpHost),
+      globs = ['assets/css/**', 'dev/sass/**'];
+
+  return gulp
+    .src(globs, { base: '.', buffer: false })
+    .pipe(conn.newer(dirHost))
+    .pipe(conn.dest(dirHost));
+}
+
+function deploy_js(){
+  let conn = ftp.create(ftpHost),
+      globs = ['assets/js/**', 'dev/js/**'];
+      
+  return gulp
+    .src(globs, { base: '.', buffer: false })
+    .pipe(conn.newer(dirHost))
+    .pipe(conn.dist(dirHost));
+}
+
+function watch(){
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  gulp.watch(["./dev/sass/*.scss", "./dev/sass/**/*.scss"], gulp.series(scss, deploy_css));
+  gulp.watch(["./dev/js/*.js", "./dev/js/**/*.js"], js);
+  gulp.watch('./*.html').on('change', browserSync.reload);
+};
+
+exports.default = watch;
